31c428896b66093287f60a8c3e3b04108a7d0f18
tatizapi <tatiana.zapirtan@yahoo.com>
2019-03-20 13:10:10 +0200

    Initial commit

---
+++ src/main/java/Lab2IC/App.java
@@ -0,0 +1,38 @@
+package Lab2IC;
+
+import Lab2IC.repository.NotaXMLRepo;
+import Lab2IC.repository.StudentXMLRepo;
+import Lab2IC.repository.TemaXMLRepo;
+import Lab2IC.service.Service;
+import Lab2IC.validation.NotaValidator;
+import Lab2IC.validation.StudentValidator;
+import Lab2IC.validation.TemaValidator;
+import Lab2IC.view.UI;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main(String[] args) {
+        StudentValidator studentValidator = new StudentValidator();
+        TemaValidator temaValidator = new TemaValidator();
+        String filenameStudent = "fisiere/Studenti.xml";
+        String filenameTema = "fisiere/Teme.xml";
+        String filenameNota = "fisiere/Note.xml";
+
+        //StudentFileRepository studentFileRepository = new StudentFileRepository(filenameStudent);
+        //TemaFileRepository temaFileRepository = new TemaFileRepository(filenameTema);
+        //NotaValidator notaValidator = new NotaValidator(studentFileRepository, temaFileRepository);
+        //NotaFileRepository notaFileRepository = new NotaFileRepository(filenameNota);
+
+        StudentXMLRepo studentXMLRepository = new StudentXMLRepo(filenameStudent);
+        TemaXMLRepo temaXMLRepository = new TemaXMLRepo(filenameTema);
+        NotaValidator notaValidator = new NotaValidator(studentXMLRepository, temaXMLRepository);
+        NotaXMLRepo notaXMLRepository = new NotaXMLRepo(filenameNota);
+        Service service = new Service(studentXMLRepository, studentValidator, temaXMLRepository, temaValidator, notaXMLRepository, notaValidator);
+        UI ui = new UI(service);
+        ui.run();
+    }
+}
---
+++ src/main/java/Lab2IC/repository/HasID.java
@@ -0,0 +1,16 @@
+package Lab2IC.repository;
+
+public interface HasID<ID> {
+
+    /**
+     *
+     * @return id-ul unui obiect
+     */
+    ID getID();
+
+    /**
+     * Modifica id-ul unul obiect
+     * @param id - noul id
+     */
+    void setID(ID id);
+}
