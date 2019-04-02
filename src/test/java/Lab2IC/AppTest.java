d2027c0381b2f8fa75a2d5a2fb6e02d70821b03c
tatizapi <tatiana.zapirtan@yahoo.com>
2019-04-02 16:39:13 +0300

    EC tests added

--- src/test/java/Lab2IC/AppTest.java
+++ src/test/java/Lab2IC/AppTest.java
@@ -65,9 +65,61 @@ public class AppTest
     }
 
     // --- EC test cases ---
+
+    //No. 2
     @Test(expected = ValidationException.class)
-    public void addStudentIdEmpty() {
+    public void addStudentIdEmptyShouldRaiseException() {
         Student student = new Student("", "Name", 10, "name@email.com");
         service.addStudent(student);
     }
+
+    //No. 3
+    @Test(expected = ValidationException.class)
+    public void addStudentIdContainsSymbolsShouldRaiseException() {
+        Student student = new Student("-1.3", "Name", 10, "name@email.com");
+        service.addStudent(student);
+    }
+
+    //No. 4
+    @Test(expected = ValidationException.class)
+    public void addStudentNameEmptyStringShouldRaiseException() {
+        Student student = new Student("id1", "", 10, "name@email.com");
+        service.addStudent(student);
+    }
+
+    //No. 5
+    @Test(expected = ValidationException.class)
+    public void addStudentNameContainsSymbolsShouldRaiseException() {
+        Student student = new Student("id1", "name*", 10, "name@email.com");
+        service.addStudent(student);
+    }
+
+    //No. 6
+    @Test(expected = ValidationException.class)
+    public void addStudentNameContainsDigitsShouldRaiseException() {
+        Student student = new Student("id1", "name2", 10, "name@email.com");
+        service.addStudent(student);
+    }
+
+    //No. 7
+    @Test(expected = ValidationException.class)
+    public void addStudentGroupIsZeroShouldRaiseException() {
+        Student student = new Student("id1", "name", 0, "name@email.com");
+        service.addStudent(student);
+    }
+
+    //No. 8
+    @Test(expected = ValidationException.class)
+    public void addStudentGroupIsNegativeShouldRaiseException() {
+        Student student = new Student("id1", "name", -2, "name@email.com");
+        service.addStudent(student);
+    }
+
+    //No. 9
+    @Test(expected = ValidationException.class)
+    public void addStudentEmailRespectsFormatShouldRaiseException() {
+        Student student = new Student("id1", "name", 10, "name");
+        service.addStudent(student);
+    }
+
 }
