7bf42830b99ae9f51aea1b820c30c09dd53e760e
tatizapi <tatiana.zapirtan@yahoo.com>
2019-05-08 09:09:27 +0300

    Top down integration testing added

--- src/test/java/Lab2IC/TopDownIntegrationTesting.java
+++ src/test/java/Lab2IC/TopDownIntegrationTesting.java
@@ -1,5 +1,8 @@
 package Lab2IC;
 
+import Lab2IC.domain.Nota;
+import Lab2IC.domain.Student;
+import Lab2IC.domain.Tema;
 import Lab2IC.repository.NotaXMLRepo;
 import Lab2IC.repository.StudentXMLRepo;
 import Lab2IC.repository.TemaXMLRepo;
@@ -9,6 +12,11 @@ import Lab2IC.validation.StudentValidator;
 import Lab2IC.validation.TemaValidator;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 public class TopDownIntegrationTesting {
 
@@ -46,21 +54,63 @@ public class TopDownIntegrationTesting {
     // Add Student
     @Test
     public void td_canAddStudent() {
+        Student student = new Student("id_stud", "TestName", 100, "testname@email.com");
+        Student savedStudent = studentXMLRepository.save(student);
 
+        assertNull(savedStudent);
+        assertEquals(studentXMLRepository.findOne("id_stud"), student);
+
+        //clean up
+        studentXMLRepository.delete("id_stud");
     }
 
 
     // Add Assignment (Add Student + Add Assignment)
     @Test
     public void td_canAddAssignment() {
+        Student student = new Student("id_stud", "TestName", 100, "testname@email.com");
+        Student savedStudent = studentXMLRepository.save(student);
 
+        assertNull(savedStudent);
+        assertEquals(studentXMLRepository.findOne("id_stud"), student);
+
+        Tema assignment = new Tema("id_tema", "TestDescription", 9, 8);
+        Tema savedAssignment = temaXMLRepository.save(assignment);
+
+        assertNull(savedAssignment);
+        assertEquals(temaXMLRepository.findOne("id_tema"), assignment);
+
+        //clean up
+        studentXMLRepository.delete("id_stud");
+        temaXMLRepository.delete("id_tema");
     }
 
 
     // Add Grade (Add Student + Add Assignment + Add Grade)
     @Test
     public void td_canAddGrade() {
+        Student student = new Student("id_stud", "TestName", 100, "testname@email.com");
+        Student savedStudent = studentXMLRepository.save(student);
 
+        assertNull(savedStudent);
+        assertEquals(studentXMLRepository.findOne("id_stud"), student);
+
+        Tema assignment = new Tema("id_tema", "TestDescription", 9, 8);
+        Tema savedAssignment = temaXMLRepository.save(assignment);
+
+        assertNull(savedAssignment);
+        assertEquals(temaXMLRepository.findOne("id_tema"), assignment);
+
+        Nota grade = new Nota("id_nota", "id_stud", "id_tema", 10, LocalDate.of(2019, 5, 8));
+        Nota savedGrade = notaXMLRepository.save(grade);
+
+        assertNull(savedGrade);
+        assertEquals(notaXMLRepository.findOne("id_nota"), grade);
+
+        //clean up
+        studentXMLRepository.delete("id_stud");
+        temaXMLRepository.delete("id_tema");
+        temaXMLRepository.delete("id_nota");
     }
 
 }
